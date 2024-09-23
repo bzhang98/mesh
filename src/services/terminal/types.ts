0063e03bf734c399ed4f5b35983865c296b12d94
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-09-23 10:58:06 -0500

    initial commit

---
+++ src/services/terminal/types.ts
@@ -0,0 +1,40 @@
+export type FileType = "file" | "directory";
+
+export interface File {
+  type: "file";
+  name: string;
+  content: string;
+}
+
+export interface Directory {
+  type: "directory";
+  name: string;
+  contents: Record<string, File | Directory>;
+}
+
+export type FileSystemNode = File | Directory;
+
+export interface FileSystem {
+  createFile: (name: string, content?: string) => void;
+  createDirectory: (name: string) => void;
+  changeDirectory: (path: string) => void;
+  listContents: () => string[];
+  getCurrentPath: () => string;
+  writeFile: (name: string, content: string, append: boolean) => void;
+  readFile: (name: string) => string;
+}
+
+export interface CommandProcessor {
+  execute: (commandString: string) => string;
+}
+
+export interface Terminal {
+  executeCommand: (commandString: string) => string;
+  getHistory: () => string[];
+}
+
+export type Command = (args: string[]) => string;
+
+export interface Commands {
+  [key: string]: Command;
+}
